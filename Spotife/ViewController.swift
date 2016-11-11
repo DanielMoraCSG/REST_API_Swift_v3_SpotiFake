ebd503bc030eebd46bb45455d4134da993d9df3b
DanielMoraCSG <daniel@csgroup.mx>
2016-11-11 17:25:13 -0600

    Initial Commit

---
+++ Spotife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spotife
+//
+//  Created by daniel on 11/11/16.
+//  Copyright © 2016 CS Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
